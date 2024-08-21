78af51db92997b5d64736d98427898074730b48f
Danilo Dias <danilolive30@hotmail.com>
2024-08-21 20:01:47 -0300

    doc: first commit and install tailwhind, tanstack query

---
+++ front/pokedex-zrp/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center space-y-7">
+      <div className="text-4xl text-yellow-400">Pokedex ZRP</div>
+      <input className="rounded-sm" placeholder="Search Pokemon" />
+    </div>
+  );
+}
+
+export default App;
