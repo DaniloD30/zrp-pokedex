3832b9f523500cc65db8a33f1d6962212bfc3a1d
Danilo Dias <danilolive30@hotmail.com>
2024-08-22 15:56:44 -0300

    Update index.tsx

--- front/pokedex-zrp/src/components/NotFound/index.tsx
+++ front/pokedex-zrp/src/components/NotFound/index.tsx
@@ -1,7 +1,7 @@
 const NotFound = () => {
   return (
     <div className="flex items-center justify-center">
-      <h3 className="text-4xl font-bold">Pokemon Not Found :(</h3>
+      <h3 className="text-4xl font-bold">Pokémon Not Found :(</h3>
     </div>
   );
 };
