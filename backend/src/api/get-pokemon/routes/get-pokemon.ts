efd502a8b3b51e8982c5a89e48d57dad16603067
Danilo Dias <danilolive30@hotmail.com>
2024-08-21 21:32:36 -0300

    first commit and generate api pokemon

---
+++ backend/src/api/get-pokemon/routes/get-pokemon.ts
@@ -0,0 +1,13 @@
+export default {
+  routes: [
+    // {
+    //  method: 'GET',
+    //  path: '/get-pokemon',
+    //  handler: 'get-pokemon.exampleAction',
+    //  config: {
+    //    policies: [],
+    //    middlewares: [],
+    //  },
+    // },
+  ],
+};
---
+++ backend/src/api/get-pokemon/services/get-pokemon.ts
@@ -0,0 +1,5 @@
+/**
+ * get-pokemon service
+ */
+
+export default () => ({});
